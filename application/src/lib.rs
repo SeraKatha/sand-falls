cd9e19a6670a93402569ae0fd5403d12b33c9ef2
Sera Katharina Litsch <sera.katharina.litsch@gmail.com>
2026-05-27 13:06:29 +0200

    Moved type Pulse into its own file

--- application/src/lib.rs
+++ application/src/lib.rs
@@ -1,4 +1,3 @@
-use std::hash;
 use std::time::{Duration, Instant};
 
 use simulation::{Cell, Grid, Simulation};
@@ -11,6 +10,8 @@ use macroquad::ui::{
 mod view;
 use view::View;
 mod tool;
+mod pulse;
+use pulse::Pulse;
 
 fn init_chunk_texture() -> Texture2D {
     let bytes =  [128u8; Simulation::CELLS_PER_CHUNK * 4];
@@ -28,36 +29,6 @@ fn cell_to_color(cell : Cell) -> [u8; 4] {
         Cell::SAND  => [242, 203, 151, 255],
         Cell::STONE => [ 93,  93,  93, 255],
         Cell::WATER => [  0,  96, 255, 255],
-    }
-}
-
-
-struct Pulse {
-    base_freq : f32,
-    speed_scale : f32,
-    time_since_last_tick : f32,
-}
-
-
-impl Pulse {
-    pub fn new(base_freq : f32) -> Self {
-        return Self { base_freq, speed_scale : 1.0, time_since_last_tick: 0.0 };
-    }
-
-    pub fn tick(&mut self, delta_time : f32) -> bool {
-        // Round delta_time to a integer frame rate to prevent stuttering from slight FPS variation e.g. 59.9
-        self.time_since_last_tick += 1.0 / (1.0 / (delta_time * self.speed_scale)).round();
-        if self.time_since_last_tick >= 1.0 / self.base_freq {
-            self.time_since_last_tick -= 1.0 / self.base_freq;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    pub fn get_speed_scale_mut(&mut self) -> &mut f32 {
-        &mut self.speed_scale
     }
 }
 
---
+++ application/src/pulse.rs
@@ -0,0 +1,28 @@
+pub struct Pulse {
+    base_freq : f32,
+    speed_scale : f32,
+    time_since_last_tick : f32,
+}
+
+
+impl Pulse {
+    pub fn new(base_freq : f32) -> Self {
+        return Self { base_freq, speed_scale : 1.0, time_since_last_tick: 0.0 };
+    }
+
+    pub fn tick(&mut self, delta_time : f32) -> bool {
+        // Round delta_time to a integer frame rate to prevent stuttering from slight FPS variation e.g. 59.9
+        self.time_since_last_tick += 1.0 / (1.0 / (delta_time * self.speed_scale)).round();
+        if self.time_since_last_tick >= 1.0 / self.base_freq {
+            self.time_since_last_tick -= 1.0 / self.base_freq;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    pub fn get_speed_scale_mut(&mut self) -> &mut f32 {
+        &mut self.speed_scale
+    }
+}
